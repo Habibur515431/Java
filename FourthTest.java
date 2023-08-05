aadac32eea61231102fbb7d75cbc0aef918edde3
Md.Habibur Rahman <141498344+Habibur515431@users.noreply.github.com>
2023-08-05 19:15:58 +0600

    java

---
+++ FourthTest.java
@@ -0,0 +1,8 @@
+public class FourthTest {
+   public static void main(String[] args){
+        int a = 15;
+        int b = 5;
+        int c = a*b;
+           System.out.println(c);
+      }
+}
---
+++ ThirdTest.java
@@ -0,0 +1,8 @@
+public class ThirdTest {
+   public static void main(String[] args){
+        int a = 50;
+        int b = 25;
+        int c = a-b;
+          System.out.println(c);
+   }
+}
